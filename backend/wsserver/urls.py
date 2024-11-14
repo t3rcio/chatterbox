1591c7fbcda792bdcf5f13e3a7364385340f1e31
Tercio <t3rcio@gmail.com>
2024-11-14 20:39:37 -0300

    Inicia modulo wsserver para websockets

---
+++ backend/wsserver/urls.py
@@ -0,0 +1,10 @@
+from tornado.web import url
+from wsserver.handlers import MainHandler
+
+route_table = [
+    url(
+        r'/chat/(?P<id>[a-zA-Z0-9]*)',
+        MainHandler,
+        name="main-handler"
+    ),
+]
