56800f8edd1aa385635f89f96c856b1a3e37ad92
Tercio <t3rcio@gmail.com>
2024-11-19 10:10:20 -0300

    Adiciona config de CORS e logging

--- backend/chatterbox/settings.py
+++ backend/chatterbox/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
+from datetime import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +127,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Chatterbox
+ALLOWED_CORS_SERVERS = '*'
+
+FORMAT_DATE = '%Y-%m-%d'
+LOG_FILES_PATH = os.path.join(BASE_DIR, '_logs')
+LOG_FILENAME = LOG_FILES_PATH + datetime.now().strftime(FORMAT_DATE) + '.log'
