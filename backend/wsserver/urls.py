22a9f82368553e7b055d30d8ec893097dc6a0905
Tercio <t3rcio@gmail.com>
2024-11-18 22:27:42 -0300

    Atualiza tratamento das urls de chats

--- backend/wsserver/urls.py
+++ backend/wsserver/urls.py
@@ -3,7 +3,7 @@ from wsserver.handlers import MainHandler
 
 route_table = [
     url(
-        r'/chat/(?P<id>[a-zA-Z0-9]*)',
+        r'/chat/(?P<id>[a-zA-Z0-9\-]*)',
         MainHandler,
         name="main-handler"
     ),
