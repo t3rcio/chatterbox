c6c37c72683a00677633d3e4bb5204380dbb8f5e
Tercio <t3rcio@gmail.com>
2024-12-10 10:19:51 -0300

    Resolve bugs

--- backend/wsserver/handlers.py
+++ backend/wsserver/handlers.py
@@ -3,11 +3,15 @@ import aiosqlite
 import json
 import logging
 
+from datetime import datetime
+from uuid import uuid4
 from django.conf import settings
 from tornado import websocket
 
 from core.models import Message
 from wsserver.settings import LOG_FILENAME
+
+HANDSHAKE = 'Chatterbox Client v.10'
 
 '''
 Ex. de Message em json
@@ -40,17 +44,34 @@ async def get_chat(id:str) -> tuple:
     '''
     (async) Obtem a conversa pelo id    
     '''
-    database = await connect()
-    cursor = await database.execute("SELECT * FROM core_chat WHERE id = '%s'" % (id,))
-    result = await cursor.fetchone()
-    await cursor.close()
+    result = ()
+    try:
+        database = await connect()
+        cursor = await database.execute("SELECT * FROM core_chat WHERE id = '%s'" % (id,))
+        result = await cursor.fetchone()
+        await cursor.close()
+    except Exception as _error:
+        logging.error(str(_error))
+    
+    return result
+
+async def get_user(id:int) -> tuple:
+    result = ()
+    try:
+        database = await connect()
+        cursor = await database.execute("SELECT * FROM auth_user WHERE id = %s" % ( int(id),))
+        result = await cursor.fetchone()
+        await cursor.close()
+    except Exception as _error:
+        logging.error(str(_error))
+    
     return result
 
 class MainHandler(websocket.WebSocketHandler):
 
     connections = []
 
-    async def check_origin(self, origin:str) -> bool:
+    def check_origin(self, origin:str) -> bool:
         '''
         Validates the origin
         # TODO
@@ -64,7 +85,7 @@ class MainHandler(websocket.WebSocketHandler):
         chat_id = message.get('chat').get('id')
         conns = [c 
             for c in self.connections 
-            if c.chat_id == chat_id
+            if c.chat_id == chat_id.replace('-','')
         ]
         return conns
 
@@ -72,7 +93,6 @@ class MainHandler(websocket.WebSocketHandler):
         chat_id = kwargs.get('id').replace('-', '')
         chat = await get_chat(id=chat_id)
         if not chat:
-            self.write_message('\{\}')
             return
         
         setattr(self, 'chat_id', chat_id)
@@ -80,19 +100,34 @@ class MainHandler(websocket.WebSocketHandler):
     
     async def on_message(self, message, *args, **kwargs):
         _message = json.loads(message)
+        chat_id = _message.get('chat').get('id')
+        user = await get_user(_message.get('user').get('id', '0'))
+        payload = {
+            'id': str(uuid4()),
+            'chat': {
+                'id': chat_id
+            },
+            'text': _message.get('text'),
+            'blob': '',
+            'user': {
+                'id': user[0],
+                'username': user[4]
+            },
+            'timestamp': datetime.now().timestamp()
+        }
         try:
             conns = await self.route_message(_message)
             for conn in conns:
-                conn.write_message(_message.get('text'))
+                conn.write_message(json.dumps(payload))
         
         except Exception as error:
             logging.error(str(error))
     
-    async def on_close(self):
-        MainHandler.connections.pop(self)
+    def on_close(self):
+        _index = MainHandler.connections.index(self)
+        MainHandler.connections.pop(_index)
         diff = str(len(MainHandler.connections))
         if settings.DEBUG:
             print("Connection close. Remaining " + diff)
-        
-        loggin.info('Connection closed. Remaining ' + diff)
+        logging.info('Connection closed. Remaining ' + diff)
 
