5769f6a33e330ed3b1c6636b6c53e400db475e9c
Tercio <t3rcio@gmail.com>
2024-12-30 22:43:38 -0300

    Habilita endpoint para remover chats

--- backend/core/views.py
+++ backend/core/views.py
@@ -176,3 +176,21 @@ def access_chat(request, chat_id):
     _response = JsonResponse(response, safe=False)
     _response[HEADER_CORS] = settings.ALLOWED_CORS_SERVERS
     return _response
+
+@api.delete('/chat/{user_id}/{chat_id}')
+def remove_chat_user(request, user_id, chat_id):
+    response = {}    
+    
+    try:
+        chat = Chat.objects.get(id=chat_id)
+        user = User.objects.get(id=int(user_id))
+        user_chat = ChatUsers.objects.filter(user=user, chat=chat)
+        if user_chat.exists():
+            user_chat.delete()
+        response = [{'id': u.id, 'username': u.username, 'email': u.email} for u in ChatUsers.users_list(chat)]        
+    except Exception as _error:
+        logging.error(_error)
+    
+    _response = JsonResponse(response, safe=False)
+    _response[HEADER_CORS] = settings.ALLOWED_CORS_SERVERS
+    return _response
